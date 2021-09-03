662679bba101447c86a7c5fd5f01b8382dc90595
Ignas Kiaulakys <ignas.kiaulakys@gmail.com>
2021-09-03 09:30:48 +0300

    Add handlebars

--- index.js
+++ index.js
@@ -2,14 +2,26 @@ const { response } = require('express');
 const express = require('express');
 const path = require('path');
 const logger = require('./middleware/logger');
+const exphbs = require('express-handlebars');
+const members = require('./Members')
 const app = express();
 
 //Init middleware
 app.use(logger);
 
+// Handlebars Middleware
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
 // Body parser middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+// Homepage Route
+app.get('/', (req, res) => res.render('index', {
+    title: 'Member app',
+    members
+}));
 
 // app.get('/', (require, response) => {
 //     // response.send('<h1>Hello World</h1>');
