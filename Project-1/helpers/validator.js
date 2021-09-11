6c14e31208bce1f40ba862a54fc93f5ea8566c54
Ignas Kiaulakys <ignas.kiaulakys@gmail.com>
2021-09-11 22:57:44 +0300

    fix folders

--- Project-1/helpers/validator.js
+++
@@ -1,8 +0,0 @@
-const Joi = require('@hapi/joi');
-
-const authSchema = Joi.object({
-    name: Joi.string().lowercase().min(2).required(),
-    email: Joi.string().email().required()
-});
-
-module.exports = authSchema;
